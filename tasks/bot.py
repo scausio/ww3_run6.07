8d3adaf7cf6fbf9ef8bee133a4f7909668d86f65
Salvatore Causio <salvatore.causio@cmcc.it>
2021-11-16 11:24:51 +0100

    adding telegram bot

--- tasks/bot.py
+++ tasks/bot.py
@@ -1,16 +1,7 @@
 import json
 import requests
 from datetime import datetime
-'''import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--message', '-m', help='message to send')
-
-args = parser.parse_args()
-
-TOKEN = '625361077:AAHytmtu-kIiSKXwwk5zuTZ4YdVy3pIKDM4'
-URL = "https://api.telegram.org/bot%s/" % (TOKEN)
-'''
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
@@ -34,14 +25,14 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 def send_message(text):
-    token = "625361077:AAHytmtu-kIiSKXwwk5zuTZ4YdVy3pIKDM4"
+    token = 'add here you token'
     address = "https://api.telegram.org/bot%s/" % (token)
-    chat_id="106157864"
+    chat_id="add here you chat id"
     url =  "{u}sendMessage?text={t}&chat_id={c}".format(u=address,t=text,c=chat_id)
     get_updates(address)
     get_url(url)
 
 
-#send_message(args.message, '106157864')
+
 
 
